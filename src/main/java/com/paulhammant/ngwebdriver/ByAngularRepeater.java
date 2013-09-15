508b2eac4e19f6539655275cf4976f83fb18a3f6
Paul Hammant <paul@hammant.org>
2013-09-15 13:50:43 -0500

    merge conflicts

--- src/main/java/com/paulhammant/ngwebdriver/ByAngularRepeater.java
+++ src/main/java/com/paulhammant/ngwebdriver/ByAngularRepeater.java
@@ -1,59 +1,83 @@
 package com.paulhammant.ngwebdriver;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-public class ByAngularRepeater extends By {
+public class ByAngularRepeater extends ByAngular.BaseBy {
 
-    public static ByAngularRepeater angularRepeater(String repeater) {
-        return new ByAngularRepeater(repeater);
+    public ByAngularRepeater(JavascriptExecutor jse, String repeater) {
+        super(jse);
+        this.repeater = repeater;
     }
 
     private String repeater;
 
-    public ByAngularRepeater(String repeater) {
-        this.repeater = repeater;
-    }
-
-    private By makeJsBy() {
-        return By.js(
-                "var using = arguments[0] || document;\n" +
-                "var repeater = '" + repeater + "';\n" +
-                "\n" +
-                "var rows = [];\n" +
-                "var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-'];\n" +
-                "for (var p = 0; p < prefixes.length; ++p) {\n" +
-                "  var attr = prefixes[p] + 'repeat';\n" +
-                "  var repeatElems = using.querySelectorAll('[' + attr + ']');\n" +
-                "  attr = attr.replace(/\\\\/g, '');\n" +
-                "  for (var i = 0; i < repeatElems.length; ++i) {\n" +
-                "    if (repeatElems[i].getAttribute(attr).indexOf(repeater) != -1) {\n" +
-                "      rows.push(repeatElems[i]);\n" +
-                "    }\n" +
-                "  }\n" +
-                "}\n" +
-                "return rows;");
-    }
-
     public ByAngularRepeaterRow row(int row) {
-        return new ByAngularRepeaterRow(repeater, row);
+        return new ByAngularRepeaterRow(jse, repeater, row);
     }
 
     public ByAngularRepeaterColumn column(String column) {
-        return new ByAngularRepeaterColumn(repeater, column);
+        return new ByAngularRepeaterColumn(jse, repeater, column);
     }
 
     @Override
     public WebElement findElement(SearchContext context) {
-        return makeJsBy().findElement(context);
+        if (context instanceof WebDriver) {
+            context = null;
+        }
+        Object o = jse.executeScript(
+                "var using = arguments[0] || document;\n" +
+                        "var repeater = '" + repeater + "';\n" +
+                        "\n" +
+                        "var rows = [];\n" +
+                        "var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-'];\n" +
+                        "for (var p = 0; p < prefixes.length; ++p) {\n" +
+                        "  var attr = prefixes[p] + 'repeat';\n" +
+                        "  var repeatElems = using.querySelectorAll('[' + attr + ']');\n" +
+                        "  attr = attr.replace(/\\\\/g, '');\n" +
+                        "  for (var i = 0; i < repeatElems.length; ++i) {\n" +
+                        "    if (repeatElems[i].getAttribute(attr).indexOf(repeater) != -1) {\n" +
+                        "      rows.push(repeatElems[i]);\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}\n" +
+                        "return rows[0];", context);
+        errorIfNull(o);
+        return (WebElement) o;
     }
 
     @Override
     public List<WebElement> findElements(SearchContext searchContext) {
-        return makeJsBy().findElements(searchContext);
+        if (searchContext instanceof WebDriver) {
+            searchContext = null;
+        }
+        Object o = jse.executeScript(
+                "var using = arguments[0] || document;\n" +
+                        "var repeater = '" + repeater + "';\n" +
+                        "\n" +
+                        "var rows = [];\n" +
+                        "var prefixes = ['ng-', 'ng_', 'data-ng-', 'x-ng-'];\n" +
+                        "for (var p = 0; p < prefixes.length; ++p) {\n" +
+                        "  var attr = prefixes[p] + 'repeat';\n" +
+                        "  var repeatElems = using.querySelectorAll('[' + attr + ']');\n" +
+                        "  attr = attr.replace(/\\\\/g, '');\n" +
+                        "  for (var i = 0; i < repeatElems.length; ++i) {\n" +
+                        "    if (repeatElems[i].getAttribute(attr).indexOf(repeater) != -1) {\n" +
+                        "      rows.push(repeatElems[i]);\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}\n" +
+                        "return rows;", searchContext);
+        errorIfNull(o);
+        return (List<WebElement>) o;
     }
 
+    @Override
+    public String toString() {
+        return "repeater(" + repeater + ')';
+    }
 }
