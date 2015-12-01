64e8f57d53aae1a94ed895a84cd157073bf060da
Paul Hammant <paul@hammant.org>
2015-11-30 21:20:53 -0500

    demo of oleaster in our codebase

---
+++ src/test/java/com/paulhammant/ngwebdriver/OleasterIntroductionTest.java
@@ -0,0 +1,17 @@
+package com.paulhammant.ngwebdriver;
+
+import com.mscharhag.oleaster.runner.OleasterRunner;
+import org.junit.runner.RunWith;
+
+import static com.mscharhag.oleaster.matcher.Matchers.expect;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.describe;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.it;
+
+@RunWith(OleasterRunner.class)
+public class OleasterIntroductionTest {{
+    describe("A suite", () -> {
+        it("contains a spec with an expectation", () -> {
+            expect(40 + 2).toEqual(42);
+        });
+    });
+}}
